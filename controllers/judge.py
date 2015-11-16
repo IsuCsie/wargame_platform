bf0e3140021ea70ab737f7b135216328f329ac9a
Jhe <ucg.jhe@gmail.com>
2015-11-17 01:28:19 +0800

    remove redudant code from judge.py

--- controllers/judge.py
+++ controllers/judge.py
@@ -2,24 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-class Reverse100Validator(object):
-    def decoder(self, plain):
-        output = ""
-        originString = plain
-        strTarget = len(originString) % 3;
-        for i in range(0, len(originString)):
-            output += chr(ord(originString[i]) - 0x3 - strTarget);
-        return output
-
-    def encoder(self, password):
-        output = ""
-        encryptedString = password
-        strTarget = len(encryptedString) % 3
-        for i in range(0, len(encryptedString)):
-            output += chr(ord(encryptedString[i]) + 0x3 + strTarget)
-        return output
-
-
 class Judger(object):
     w100_key = "ISU{comment_is_very_important}"
     w200_key = "ISU{jsfuck_is_rock!}"
